753fc9fb5e0512d1a228832040d917bd43460202
blocka025 <110356349+blocka025@users.noreply.github.com>
2023-11-09 14:17:39 -0600

    Add files via upload

--- MonitoringFormatter.py
+++ MonitoringFormatter.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import time
+
 us_state_to_abbrev = {
     "Alabama": "AL",
     "Alaska": "AK",
@@ -62,35 +63,114 @@ us_state_to_abbrev = {
     "U.S. Virgin Islands": "VI",
 }
 
-folders_path = 'C:/Users/blake/Documents/VSCode/Python/Hope/Cision Files/'
-output_path = folders_path
+def get_not_nan_count(row):
+    count = 0
+    for r in row:
+        if str(r) != 'nan':
+            count +=1
+    return count
+    
+def get_start(data):
+    for i in range(len(data)):
+        if get_not_nan_count(data[i]) > 5:
+            return i
+def get_data_rows(data):
+    s = get_start(data)
+    output = []
+    for i in range(len(data)):
+        if i != s:
+            if get_not_nan_count(data[i]) > 5:
+                output.append(data[i])
+    return np.array(output)
 
-cheat_sheet = np.loadtxt(folders_path+'../'+'FormattingCheatSheet.txt', delimiter='|', skiprows=2,dtype=str)
+
+folders_path = 'Cision Files/'
+output_path = 'Formatted Files/'
+
+cheat_sheet = np.loadtxt('FormattingCheatSheet.txt',
+                         delimiter='|',
+                         skiprows=2,
+                         dtype=str)
+cheat_sheet2 = np.loadtxt('NewFormattingCheatSheet.txt',
+                         delimiter='|',
+                         skiprows=2,
+                         dtype=str)
+cheat_sheet3 = np.loadtxt('CriticalMentionFormatting.txt',
+                         delimiter='|',
+                         skiprows=2,
+                         dtype=str)
 cheat_dict = {}
 for row in cheat_sheet:
     cheat_dict[row[1].strip()] = row[0].strip()
+for i, row in enumerate(cheat_sheet2):#inport other cheatsheet
+    if i < len(cheat_sheet2)-4 and row[1].strip() in cheat_dict and cheat_dict[row[1].strip()] != row[0].strip():
+        print(row[1].strip() + ' is being overwritten with '+row[0].strip())
+    cheat_dict[row[1].strip()] = row[0].strip()
+for i, row in enumerate(cheat_sheet3):#inport third cheatsheet
+    if i < len(cheat_sheet3)-4 and row[1].strip() in cheat_dict and cheat_dict[row[1].strip()] != row[0].strip():
+        print(row[1].strip() + ' is being overwritten with '+row[0].strip())
+    cheat_dict[row[1].strip()] = row[0].strip()
+    
 folder_name = input('Input Folder Name: ')
 try:
-    files = os.listdir(folders_path+folder_name)
+    files = os.listdir(folders_path + folder_name)
 except:
     print(folder_name + ' not found :(')
     exit()
 
 if len(files) == 0:
-    print('No files in '+folder_name)
+    print('No files in ' + folder_name)
     exit()
 elif len(files) > 1:
-    print('Too many Cision files in '+folder_name+'. Remove all but one file')
+    print('Too many Cision files in ' + folder_name +
+          '. Remove all but one file')
     exit()
 
-
 print('File found!')
-input_dat = pd.read_excel(folders_path+folder_name+'/'+files[0])
-input_dat = np.array(input_dat)
-input_entries = np.transpose(input_dat[4:])
+path = folders_path + folder_name + '/' + files[0]
+if path[-3:] == 'csv':
+    input_dat = pd.read_csv(path,header=None)
+elif path[-4:] == 'xlsx' or path[-3:] == 'xls':
+    input_dat = pd.read_excel(folders_path + folder_name + '/' + files[0])
+else:
+    print('File type not recognized')
+    exit()
+
+input_dat = list(np.array(input_dat))
+input_dat2 = get_data_rows(input_dat)
+input_entries = np.transpose(input_dat2)
 # print(input_entries)
-total_hits = int(input_dat[0][1])
-cols = input_dat[2]
+# total_hits = int(input_dat[0][1])
+cols = input_dat[get_start(input_dat)]
+# print(cols)
+for i, col in enumerate(cols):
+    # print(col)
+    if str(col) =='nan':
+        cols[i] = 'nan'
+    else:
+        cols[i] = col.strip()
+
+while True:
+    my_str = input('Show Available Columns? (y/n) : ')
+    if my_str == 'y':
+        for col in cols:
+            print(col)    
+        break
+    elif my_str == 'n':
+        break
+    else:
+        print('Please type either "y" or "n"')
+sort_states = False
+while 'State' in cols:
+    my_str = input('Sort by northern Midwestern states? (y/n) : ')
+    if my_str == 'y':
+        sort_states = True   
+        break
+    elif my_str == 'n':
+        break
+    else:
+        print('Please type either "y" or "n"')
+
 
 #input formatting
 my_str = 'Default'
@@ -100,52 +180,63 @@ while True:
     print()
     print(output_str)
     my_str = input('Input Column Heading Shortcut: ').strip()
-    if my_str in cheat_dict:
-        if cheat_dict[my_str] == 'Finish':
-            if my_str == 'I love Blake':
-                print('\nAww love you too <3\n')
-                time.sleep(2)
-            break
-        elif cheat_dict[my_str] == 'Blank Column' or cheat_dict[my_str] == 'Topic' or cheat_dict[my_str] == 'Notes':
-            output_cols.append('None')
-            output_str += cheat_dict[my_str] + '|'
-        elif cheat_dict[my_str] == 'Market':
-            output_cols.append('M')
-            output_str += cheat_dict[my_str] + '|'
-        elif cheat_dict[my_str] == 'Delete Previous':
-            if len(output_cols) > 0:
-                output_cols.pop()
-                output_str = output_str[:-1]
-                while output_str[-1] != '|':
+    try:
+        if my_str in cheat_dict:
+            if cheat_dict[my_str] == 'Finish':
+                if my_str == 'I love Blake':
+                    print('\nAww love you too <3\n')
+                    time.sleep(2)
+                break
+            elif cheat_dict[my_str] == 'Blank Column' or cheat_dict[my_str] == 'Topic' or cheat_dict[my_str] == 'Notes':
+                output_cols.append('None')
+                output_str += cheat_dict[my_str] + '|'
+            elif cheat_dict[my_str] == 'Market':
+                output_cols.append('M')
+                output_str += cheat_dict[my_str] + '|'
+            elif cheat_dict[my_str] == 'Delete Previous':
+                if len(output_cols) > 0:
+                    output_cols.pop()
                     output_str = output_str[:-1]
+                    while output_str[-1] != '|':
+                        output_str = output_str[:-1]
+            else:
+                output_cols.append(np.where(cols == cheat_dict[my_str])[0][0])
+                output_str += cheat_dict[my_str] + '|'
         else:
-            output_cols.append(np.where(cols == cheat_dict[my_str])[0][0])
-            output_str += cheat_dict[my_str] + '|'
-    else:
-        print('Shortcut not found.')
+            print('Shortcut not found.')
+    except IndexError:
+        print('The column heading "'+cheat_dict[my_str]+ '" could not be found')
 
 #make output structure
 row_count = len(input_entries[0])
 col_count = len(output_cols)
-output_array = [] #this is the transpose of the true output
+output_array = []  #this is the transpose of the true output
 for i, col in enumerate(output_cols):
     if col == 'M':
-        output_array.append([])
-        c = np.where(cols == 'Country')[0][0]
-        s = np.where(cols == 'State')[0][0]
-        t = np.where(cols == 'City')[0][0]
-        for j in range(row_count):
-            if input_entries[c][j] != 'United States':
-                output_array[i].append(input_entries[c][j])
-            else:
-                if str(input_entries[t][j]) == 'nan' and str(input_entries[t][j]) == 'nan':
-                    output_array[i].append('United States')
-                elif str(input_entries[t][j]) == 'nan':
-                    output_array[i].append(input_entries[s][j])
+        try:
+            output_array.append([])
+            c = np.where(cols == 'Country')[0][0]
+            s = np.where(cols == 'State')[0][0]
+            t = np.where(cols == 'City')[0][0]
+            for j in range(row_count):
+                if input_entries[c][j] != 'United States':
+                    output_array[i].append(input_entries[c][j])
                 else:
-                    output_array[i].append(str(input_entries[t][j])+', '+ us_state_to_abbrev[str(input_entries[s][j])])
+                    if str(input_entries[t][j]) == 'nan' and str(
+                            input_entries[t][j]) == 'nan':
+                        output_array[i].append('United States')
+                    elif str(input_entries[t][j]) == 'nan':
+                        output_array[i].append(input_entries[s][j])
+                    else:
+                        output_array[i].append(
+                            str(input_entries[t][j]) + ', ' +
+                            us_state_to_abbrev[str(input_entries[s][j])])
+        except:
+            output_array.append([])
+            for j in range(row_count):
+                output_array[i].append(input_entries[col][j])
     elif col == 'None':
-        output_array.append(['']*row_count)
+        output_array.append([''] * row_count)
     else:
         output_array.append([])
         for j in range(row_count):
@@ -156,9 +247,32 @@ column_headings.pop()
 for i in range(len(column_headings)):
     if column_headings[i] == 'Blank Column':
         column_headings[i] = ''
-df = pd.DataFrame(np.transpose(output_array), columns = column_headings)
+if sort_states:
+    final_out = []
+    output_array = np.transpose(output_array)
+    s = np.where(cols == 'State')[0][0]
+    for i in range(row_count):
+        if input_entries[s][i] == 'Minnesota' or input_entries[s][i] == 'Wisconsin' or input_entries[s][i] == 'North Dakota' or input_entries[s][i] == 'South Dakota' or input_entries[s][i] == 'Iowa':
+            final_out.append(output_array[i])
+    for i in range(row_count):
+        if input_entries[s][i] != 'Minnesota' and input_entries[s][i] != 'Wisconsin' and input_entries[s][i] != 'North Dakota' and input_entries[s][i] != 'South Dakota' and input_entries[s][i] != 'Iowa':
+            final_out.append(output_array[i])
+    output_array = np.transpose(final_out)
+df = pd.DataFrame(np.transpose(output_array), columns=column_headings)
 output_filename = input('Output file name: ')
-if output_filename[-5:] == '.xlsx':
-    df.to_excel(output_path+output_filename, index=False)
-else:
-    df.to_excel(output_path+output_filename+'.xlsx', index=False)
+df.to_csv(output_path + output_filename+'.csv', index=False)
+    
+
+print()
+while True:
+    my_str = input('Would you like remove old Cision file? (y/n): ')
+    if my_str == 'y':
+        os.remove(folders_path + folder_name + '/' + files[0])
+        break
+    elif my_str == 'n':
+        break
+    else:
+        print('Please type either "y" or "n"')
+print('Success!! :)')
+time.sleep(3)
+os.system('clear')
---
+++ README.txt
@@ -0,0 +1,27 @@
+Hey Hope!!
+
+This is a quick guide to my little software dohickey. You can completely ignore any files that end in .py. Those are the things that I coded. What is nice about this solution is that you do not have to download anything, and I can also edit/update the program remotely! Bug fixes and new features should be easy.
+
+Step 1:
+Open the Cision Files folder on the left side of the screen under the Files tab. Either create a new folder, or add the downloaded Cision file into the appriate folder (e.g. Minnesota Grown). Do this by selecting the three dots just to the right of the Files tab and select "Upload File". You may have to drag it into the correct folder within the Files tab.
+
+Step 2:
+Hit the big green Run button at the top of your screen. Now the program will start.
+
+Step 3:
+Next open FormattingCheatSheet.txt. This is open in the center of your screen. It may be useful to drag FormattingCheatSheet.txt from the center window to the upper right window, so you can have both files visible at the same time. (Do this by grabbing the header and dragging and dropping)
+
+Step 4:
+The bottom right of the screen is your Console. This is where the magic happens. Click anywhere in this window before typing in a command (the box should be solid, not hollow). 
+
+Step 5: 
+The first prompt asks for the input folder. This is the subfolder that you made in the Cision Files Folder in the files tab. For example, you can type "Minnesota Grown", then enter. This will look at all the files within the Minnesota Grown Folder. (Be very careful about spelling and capitalization) Note, right now I only have it set to work when there is a single file in each subfolder, so you will have to remove Cision files once you are done. 
+
+Step 6: 
+This is now the formatting input menu. The current format is displayed at the top (intially empty). Using the FormattingCheatSheet.txt as a reference, type in the format of each column that you want in order (e.g. type "D" then enter for Date). There is a "Delete Previous" option!!
+
+Step 7:
+Repeat Step 6 until you have all the formatting you want. The left column in FormattingCheatSheet.txt must be the column heading as read from the Cision file, or else my program will not be able to find the column, so those cannot change. However, if you for any reason want to change any of the shortcuts, feel free! Change any of them to whatever best suits you. After any changes though, you will have to rerun the program, by hitting Stop and then running it again.
+
+Step 8:
+Last Step!! Finally type in whatever you want to call the output file and hit enter! The file will be made and put into the Formatted Files. Find it, and click the three dots to find its Download option.
